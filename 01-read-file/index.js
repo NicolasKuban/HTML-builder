37425364ac3f3ef459b1d843f78b853c70a57fd8
NicolasKuban <nicrad@yandex.ru>
2024-01-17 19:00:58 +0300

    feat: Реализована задача 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+let __dirpath = path.resolve(path.dirname(require.main.filename))
+let __file = path.join(__dirpath, 'text.txt');
+var stream = fs.ReadStream(__file, { encoding: 'utf-8' });
+stream.on('readable', function () {
+  var data = stream.read();
+  if (data !== null) {
+      console.log(data);
+  }
+});
+ 
+// stream.on('end', function () {
+//   console.log('THE END');
+// });
