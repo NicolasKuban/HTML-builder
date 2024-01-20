4f45cbbc55b39b8be6e82f5e4888c5c8568502cb
NicolasKuban <nicrad@yandex.ru>
2024-01-20 17:37:40 +0300

    feat: Реализована функция copyDir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,23 +1,29 @@
 const fs = require('fs')
 const path = require('path')
-const __root = path.resolve(path.dirname(require.main.filename))
-const __dirSrc = path.join(__root, 'files');
-const __dirDist = path.join(__root, 'files-copy');
 
+function copyDir (dirSrc) {
+    const __root = path.resolve(path.dirname(require.main.filename))
+    const __dirSrc = path.join(__root, dirSrc);
+    const __dirDist = path.join(__root, dirSrc.concat('-copy'));
 
-fs.readdir(__dirSrc, {withFileTypes: true}, (err, files) => {
-    if (err) throw err
-    for (const file of files) {
-        if (file.isFile()) {
-            fs.stat(path.resolve(__dirSrc, file.name), (err, fileStat) => {
-                fs.copyFile(path.resolve(__dirSrc, file.name), path.resolve(__dirDist, file.name), err => {
-                    if (err) throw err
+    fs.mkdir(__dirDist, {recursive: true}, err => {
+        if (err) throw err
+    })
+
+    fs.readdir(__dirSrc, {withFileTypes: true}, (err, files) => {
+        if (err) throw err
+        for (const file of files) {
+            if (file.isFile()) {
+                fs.stat(path.resolve(__dirSrc, file.name), (err, fileStat) => {
+                    fs.copyFile(path.resolve(__dirSrc, file.name), path.resolve(__dirDist, file.name), err => {
+                        if (err) throw err
+                    })
                 })
-            })
+            }
         }
-    }
-})
+    })
+    // console.log(dirSrc)
+    // console.log(dirSrc.concat("-copy"))
+}
 
-fs.mkdir(__dirDist, {recursive: true}, err => {
-    if (err) throw err
-})
+copyDir("files")
