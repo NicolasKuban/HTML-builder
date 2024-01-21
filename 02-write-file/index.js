0d9ee2b4bc316f4bbb26104a0c7ac9a8dfa529cb
NicolasKuban <nicrad@yandex.ru>
2024-01-22 00:08:31 +0300

    refactor: Проверка линтером 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,50 +1,39 @@
-const readline = require("readline");
-const fs = require('fs')
-const path = require('path')
-let __dirpath = path.resolve(path.dirname(require.main.filename))
-let __file = path.join(__dirpath, 'text.txt');
+const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
 
-let ws = fs.createWriteStream(__file);
+const folderPath = path.resolve(path.dirname(require.main.filename));
+const filePath = path.join(folderPath, 'text.txt');
+
+const ws = fs.createWriteStream(filePath);
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-
-// fs.stat(__file, function(err, stat) {
-//     if (err === null) {
-//     }
-// })
-
-fs.unlink(__file, (err) => {
-    if(err) {
-        return
-    }
-    // throw err;
-});
-fs.open(__file, 'w', (err) => {
-    if(err) throw err;
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.on('line', (input) => {
-    if (input.toLowerCase() === 'exit') {
-        exitStream()
-    } else {
-        ws.write(input)
-        ws.write('\n')
-    }
-})
-
-rl.on('SIGINT', () => {
-    ws.write(rl.line)
-    exitStream()
+fs.open(filePath, 'w', (err) => {
+  if (err) throw err;
 });
 
-function exitStream () {
-    console.log("Stream is closed, data save to text.txt")
-    rl.close();
-    ws.close()
+function exitStream() {
+  console.log('Stream is closed, data save to text.txt');
+  rl.close();
+  ws.close();
 }
 
-console.log('Hello, my friend. Tell us about your achievements.')
-console.log('To exit, type the word "exit"')
+rl.on('line', (input) => {
+  if (input.toLowerCase() === 'exit') {
+    exitStream();
+  } else {
+    ws.write(input);
+    ws.write('\n');
+  }
+});
+
+rl.on('SIGINT', () => {
+  ws.write(rl.line);
+  exitStream();
+});
+
+console.log('Hello, my friend. Tell us about your achievements.');
+console.log('To exit, type the word "exit"');
